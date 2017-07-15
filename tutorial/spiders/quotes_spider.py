a5134d0f8defb37d3a0a19deb09c07655c7282f0
Daniel Castellani <dancastellani@gmail.com>
2017-07-15 13:58:35 -0400

    A shortcut for creating Requests

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -11,11 +11,9 @@ class QuotesSpider(scrapy.Spider):
         for quote in response.css('div.quote'):
             yield {
                 'text': quote.css('span.text::text').extract_first(),
-                'author': quote.css('small.author::text').extract_first(),
+                'author': quote.css('span small::text').extract_first(),
                 'tags': quote.css('div.tags a.tag::text').extract(),
             }
 
-        next_page = response.css('li.next a::attr(href)').extract_first()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+        for a in response.css('li.next a'):
+            yield response.follow(a, callback=self.parse)
