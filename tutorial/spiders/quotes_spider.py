c0d566f99cc6d83eadf866659cf0584de844b0ed
Daniel Castellani <dancastellani@gmail.com>
2017-07-15 13:48:46 -0400

    Following links

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -5,7 +5,6 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
         'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
     ]
 
     def parse(self, response):
@@ -15,3 +14,8 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').extract_first(),
                 'tags': quote.css('div.tags a.tag::text').extract(),
             }
+
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
